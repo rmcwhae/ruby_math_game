6dcd9ec563099cdcefa3e7fcd4d6cfc40f7bed7f
Russell McWhae <russell.mcwhae@gmail.com>
2019-09-16 18:31:34 +0000

    Separates player class into its own file

--- main.rb
+++ main.rb
@@ -1,23 +1,35 @@
-class Player
-  attr_accessor :lives
-  attr_reader :id
-  def initialize(id)
-    @id = id
-    @lives = 3
+require './player'
+
+locale = :en
+
+ALL_LANGUAGES = {
+  en: {
+    new_turn: '----- NEW TURN -----'
+  },
+  fr: {
+    new_turn: '----- NOUVEAU TOUR -----'
+  }
+}
+
+MESSAGES = ALL_LANGUAGES[locale]
+
+class Display
+  def self.say(msg)
+    puts msg
   end
 end
 
 class Game
-  def initialize
-    @player_one = Player.new(1)
-    @player_two = Player.new(2)
+  def initialize(p1, p2)
+    @player_one = p1
+    @player_two = p2
     @current_player = @player_one
   end
 
   def turn(player)
     x = rand(20)
     y = rand(20)
-    puts '----- NEW TURN -----'
+    Display.say MESSAGES[:new_turn]
     puts "Player #{player.id}: What does #{x} plus #{y} equal?"
     answer = gets.chomp.to_i
     if answer == (x + y)
@@ -29,31 +41,28 @@ class Game
     puts "P1: #{@player_one.lives}/3 vs P2: #{@player_two.lives}/3"
   end
 
-  def end_game(player)
+  def end_game
+    player =  @player_one.lives.zero? ? @player_two : @player_one
     puts '----- GAME OVER -----'
     puts "Player #{player.id} wins with a score of #{player.lives}/3"
   end
 
   def play
-    while @player_one.lives > 0 || @player_two.lives > 0
-      if @player_one.lives == 0
-        end_game(@player_two)
-        exit
-      elsif @player_two.lives == 0
-        end_game(@player_one)
-        exit
-      end
+    while @player_one.lives.positive? && @player_two.lives.positive?
       if @current_player == @player_one
         turn(@player_one)
         @current_player = @player_two
-      elsif @current_player == @player_two
+      else
         turn(@player_two)
         @current_player = @player_one
       end
     end
+    end_game
   end
 end
 
 # Action time!
-new_game = Game.new
+player_one = Player.new(1)
+player_two = Player.new(2)
+new_game = Game.new(player_one, player_two)
 new_game.play
---
+++ player.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Player
+  attr_accessor :lives
+  attr_reader :id
+  def initialize(id)
+    @id = id
+    @lives = 3
+  end
+end
