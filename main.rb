8e28928ef6619a3c5f5151da28a3a52082361ce8
Russell McWhae <russell.mcwhae@gmail.com>
2019-09-16 16:49:30 +0000

    Working player turn

--- main.rb
+++ main.rb
@@ -4,34 +4,44 @@ class Player
   attr_accessor :lives
   attr_reader :id
   def initialize(id)
-    @player_id = id
+    @id = id
     @lives = 3
   end
 end
 
-class Turn
-  def initialize(player_id)
-    x = rand(20)
-    y = rand(20)
-    puts "Player #{player_id}: What does #{x} plus #{y} equal?"
-    answer = gets.chomp.to_i
-    puts answer.inspect
-    if answer == (x + y)
-      puts "Player #{player_id}: YES! You are correct."
-    elsif answer != x + y
-      puts "Player #{player_id}: Seriously? No!"
-      # REDUCE LIVES HERES
-    end
-  end
-end
+# class Turn
+
+# end
 
 class Game
   def initialize
-    puts Turn.new(1)
+    @player_one = Player.new(1)
+    @player_two = Player.new(2)
+    @current_player = @player_one
+    # puts Turn.new(@current_player)
   end
+
+  def turn(player)
+    x = rand(20)
+    y = rand(20)
+    puts "Player #{player.id}: What does #{x} plus #{y} equal?"
+    answer = gets.chomp.to_i
+    if answer == (x + y)
+      puts "Player #{player.id}: YES! You are correct."
+    elsif answer != x + y
+      puts "Player #{player.id}: Seriously? No!"
+      player.lives -= 1
+    end
+    puts "P1: #{@player_one.lives}/3 vs P2: #{@player_two.lives}/3"
+  end
+
+  def play()
+    turn(@player_one)
+  end
+
 end
 
-player_one = Player.new(1)
-player_two = Player.new(2)
 
-puts Turn.new(2)
+new_game = Game.new
+new_game.play
+# puts new_game.turn(2)
